8c49505afa17d55d1bf2660506e3ab1ff43db5a3
sviatoslav <slavkodydych@gmail.com>
2019-05-01 16:27:06 +0300

    first commit

---
+++ src/main/java/com/homelab/romrom/RomromApplication.java
@@ -0,0 +1,21 @@
+package com.homelab.romrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RomromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomromApplication.class, args);
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(){
+        return "Hi!";
+    }
+
+}
---
+++ src/test/java/com/homelab/romrom/RomromApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homelab.romrom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RomromApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
